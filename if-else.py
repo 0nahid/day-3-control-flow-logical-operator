a70f417530b9c9648ea241ad78ab7bd6fdb4c157
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-03 22:19:49 +0600

    added if else & .vscode

---
+++ if-else.py
@@ -0,0 +1,16 @@
+print("Welcome to the rollercoster")
+height = int(input("How tall are you in cm? "))
+if height >= 182:
+    print("You're ready to go")
+    print("Enjoy the ride!")
+else:
+    print("Sorry you are a teenager")
+
+# operators && meaning
+# >            greater than
+# >=           greater than or equal to
+# <            less than
+# <            less than or equal to
+# ==           equal to
+# =!           not equal
+
